fa133df665f5b64abddce9eb7627243707ff0c9d
Victoria Tsai <67884079+victoria-p-tsai@users.noreply.github.com>
2023-10-15 03:31:56 -0400

    Add files via upload

---
+++ eco jump/constants.js
@@ -0,0 +1,20 @@
+// board
+const boardWidth = window.innerWidth;
+const boardHeight = window.innerHeight;
+
+// physics
+const initialVelocityY = -7.5; //inital game drop velocity
+const gravity = 0.3;
+
+// sound
+const bgMusicSrc = "./assets/sound/music.mp3";
+const jumpSoundSrc = "./assets/sound/jump.mp3";
+
+// character images
+const characterLightRightImgSrc = "./assets/images/character/right-char.png";
+const characterLightLeftImgSrc = "./assets/images/character/left-char.png";
+const characterDarkRightImgSrc = "./assets/images/character/right-char.png";
+const characterDarkLeftImgSrc = "./assets/images/character/left-char.png";
+
+// platform images
+const platformImgSrc = "./assets/images/platforms/platform.png";
